877caf437c146820fc755b056c32ad85e662ab11
Batuhan <bakkaya@ekin.com>
2024-04-25 09:10:24 +0300

    DTASK-2404-00038

--- frappe/public/js/frappe/form/sidebar/attachments.js
+++ frappe/public/js/frappe/form/sidebar/attachments.js
@@ -152,7 +152,7 @@ frappe.ui.form.Attachments = class Attachments {
 				if(this.frm.doctype == "Employee") {
 					show_price = frappe.user.has_role("HR Manager");
 				} else if (this.frm.doctype == "Item"){
-					if (frappe.user.has_role("Item Manager") || frappe.user.has_role("Purchase Master Manager") || frappe.user.has_role("Purchase Manager") || frappe.user.has_role("Mechanic Department Manager") || frappe.user.has_role("Electronic Department Manager") || frappe.user.has_role("Electronic Design")) {
+					if (frappe.user.has_role("Quality Department Manager") || frappe.user.has_role("Item Manager") || frappe.user.has_role("Purchase Master Manager") || frappe.user.has_role("Purchase Manager") || frappe.user.has_role("Mechanic Department Manager") || frappe.user.has_role("Electronic Department Manager") || frappe.user.has_role("Electronic Design")) {
 						show_price = true;
 					}
 				} else {
